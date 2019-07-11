a1a079315d9b1f407def77dce08347057d9b5d19
NightcoreSergeant <tomazic.tadej@gmail.com>
2019-07-11 10:24:25 +0200

    more

--- merge_sort.py
+++ merge_sort.py
@@ -14,19 +14,15 @@ def merge(alist):
             else:
                 alist[k]=r[j]
                 j=j+1
-            print('change1',l)
             k=k+1
         while i<len(l):
             alist[k]=l[i]
             i=i+1
             k=k+1
-            print('change2',l)
         while j<len(r):
             alist[k]=r[j]
             j=j+1
             k=k+1
-            print('change3',l)
-    return l
 
 #l=[random.randrange(0,10) for i in range(10**1)]
 #x=[i for i in l]
